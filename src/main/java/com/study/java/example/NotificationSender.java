a0ade80be4a9d52a2e7736f73689f7d43da18184
heesk0223@gmail.com <heesk0223@gmail.com>
2026-05-14 18:13:15 +0900

    restart

--- src/main/java/com/study/java/example/NotificationSender.java
+++
@@ -1,9 +0,0 @@
-package com.study.java.example;
-
-/**
- * 알림 발송 규약
- */
-public interface NotificationSender {
-
-    void send(String message);
-}
--- src/main/java/com/study/java/example/Refundable.java
+++
@@ -1,9 +0,0 @@
-package com.study.java.example;
-
-/**
- * 환불 기능 규약
- */
-public interface Refundable {
-
-    void refund(int amount);
-}
